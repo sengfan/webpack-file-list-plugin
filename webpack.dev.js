9f1998e9004bc0b7b748a6fcee933fa2ba90655c
Zhou Fang <zfang@crateandbarrel.com>
2019-07-19 15:58:08 -0500

    init

---
+++ webpack.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'development',
+    entry: {
+        main1: ['./src/scss/basic.scss', './src/scss/basic2.scss']
+        /*     singleFile: './src/scss/basic2.scss',
+        importFile: './src/scss/index.scss' */
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            /**
+             * Running Babel on JS files.
+             */
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: ['lodash'],
+                        presets: ['@wordpress/default']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader?-url',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()]
+};
